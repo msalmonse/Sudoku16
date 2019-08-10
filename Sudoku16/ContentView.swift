9284cd5a7ff0685863ceb53ae6ea9a98f069c95a
Michael Salmon <msalmonse@gmail.com>
2019-08-10 14:59:19 +0200

    Initial Commit

---
+++ Sudoku16/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sudoku16
+//
+//  Created by Michael Salmon on 2019-08-10.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
