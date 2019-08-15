c982ae0cffd35a7934e06149429967304fef1449
Michael Salmon <msalmonse@gmail.com>
2019-08-15 12:26:54 +0200

    Added swapping of square rows and columns

--- Sudoku16/Models/RandomizeSolution.swift
+++ Sudoku16/Models/RandomizeSolution.swift
@@ -57,6 +57,30 @@ extension Board {
         let b = (a & ~0x30) + Int.random(in: 0...3) << 4 // Random column in the same square
         if a != b { swapRows(a, b) }
     }
+    
+    // Swap all the columns between two columns of squares
+    func swapSquaresInColumn(_ square: Int) {
+        let a = square << 2
+        let b = Int.random(in: 0...3) << 2
+        if a != b {
+            swapColumns(a, b)
+            swapColumns(a + 1, b + 1)
+            swapColumns(a + 2, b + 2)
+            swapColumns(a + 3, b + 3)
+        }
+    }
+    
+    // Swap all the rows between two rows of squares
+    func swapSquaresInRow(_ square: Int) {
+        let a = square << 6
+        let b = Int.random(in: 0...3) << 6
+        if a != b {
+            swapRows(a, b)
+            swapRows(a + 16, b + 16)
+            swapRows(a + 32, b + 32)
+            swapRows(a + 48, b + 48)
+        }
+    }
 
     func randomizeSolution() {
         let digitSwap = [0] + Array(1...16).shuffled()
@@ -65,5 +89,7 @@ extension Board {
             swapColumnsInSquare(Int.random(in: 0...15))
             swapRowsInSquare(Int.random(in: 0...15) << 4)
         }
+        swapSquaresInColumn(Int.random(in: 0...3))
+        swapSquaresInRow(Int.random(in: 0...3))
     }
 }
