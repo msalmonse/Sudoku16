75608085ed11b0ce1d61bbb765e5bceffc38678a
Michael Salmon <msalmonse@gmail.com>
2019-08-19 11:17:13 +0200

    Added more settings for styling

--- Sudoku16/Views/ButtonText.swift
+++ Sudoku16/Views/ButtonText.swift
@@ -12,19 +12,34 @@ import SwiftUI
 
 struct ButtonText: View {
     let text: String
-    
+    let font: Font
+    let color: Color
+    let width: CGFloat
+    let height: CGFloat
+
+    init(_ text: String, font: Font = .title, color: Color = .primary) {
+        self.text = text
+        self.font = font
+        self.color = color
+
+        switch font {
+        case .headline: width = 100; height = 35
+        case .body:     width = 100; height = 30
+        default:        width = 150; height = 50
+        }
+    }
+
     var body: some View {
         Text(text)
-        .font(.title)
-        .foregroundColor(.primary)
+        .font(font)
+        .foregroundColor(color)
         .padding()
-        .frame(width: 150, height: 50)
+        .frame(width: width, height: height)
         .overlay(strokedCapsule())
         .background(linearBackgroupd)
         .clipShape(Capsule())
     }
 }
-
 
 fileprivate let r = 0.00
 fileprivate let g = 0.75
@@ -50,7 +65,7 @@ fileprivate let linearBackgroupd = LinearGradient(
 #if DEBUG
 struct ButtonText_Previews: PreviewProvider {
     static var previews: some View {
-        ButtonText(text: "Hello World")
+        ButtonText("Hello World")
     }
 }
 #endif
